d119e8fd077b86a14116ed17dfb2dce5200c7322
Apoorve73 <apoorve73@gmail.com>
2021-05-09 03:03:34 +0530

    Update Sign in Route

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../bootstrap/css/bootstrap.min.css'
+import * as routes from "../constants/routes";
 
 const LandingPage = () => (
   <div className="jumbotron">
@@ -8,7 +9,7 @@ const LandingPage = () => (
   <hr className="my-4" />
   <p>Fill in your details and get instant insurance in your wallet.</p>
   <p className="lead">
-    <a className="btn btn-primary btn-lg" href="signin" role="button">Get Started</a>
+    <a className="btn btn-primary btn-lg" href="https://apoorve73.github.io/signin" role="button">Get Started</a>
   </p>
   </div>
 );
