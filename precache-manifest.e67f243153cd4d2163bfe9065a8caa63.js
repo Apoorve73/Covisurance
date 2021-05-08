e1f813057368898d5366f899bc03c85f86e464a9
Apoorve73 <apoorve73@gmail.com>
2021-05-09 02:54:15 +0530

    Updates

---
+++ precache-manifest.e67f243153cd4d2163bfe9065a8caa63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1e83c0022dfd2a8544c2e3f98ca4d24",
+    "url": "/Covisurance/static/media/back.c1e83c00.png"
+  },
+  {
+    "revision": "13d2a17ed52fe59ef675",
+    "url": "/Covisurance/static/js/runtime~main.13d2a17e.js"
+  },
+  {
+    "revision": "ad77ae37aa19bd03b44b",
+    "url": "/Covisurance/static/js/main.ad77ae37.chunk.js"
+  },
+  {
+    "revision": "728d577fe845eebf005a",
+    "url": "/Covisurance/static/js/1.728d577f.chunk.js"
+  },
+  {
+    "revision": "ad77ae37aa19bd03b44b",
+    "url": "/Covisurance/static/css/main.0138a429.chunk.css"
+  },
+  {
+    "revision": "d9abee1ab5db599111a96b583a23236b",
+    "url": "/Covisurance/index.html"
+  }
+];
